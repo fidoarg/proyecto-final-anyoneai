d92aa72d1e8b048a9b74b63ede508f42ca13256f
William David López Pasuy <wdpasuy@gmail.com>
2022-11-14 16:23:54 -0500

    Backend receiving basic user info from HTML

--- api/main.py
+++ api/main.py
@@ -1,12 +1,16 @@
+import uvicorn
 import json
 
 # Fast API utilities
-from fastapi import FastAPI, Request, Query, Body
+from fastapi import FastAPI, Request, Query, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-# Pydantic tilities
+# Function to send and receive ml jobs
+from middleware import model_predict
+
+# Pydantic utilities
 from pydantic import BaseModel, Field
 
 # Create API
@@ -21,33 +25,76 @@ templates = Jinja2Templates(directory="templates")
 
 # Application
 class Application(BaseModel):
-    sex: str = Query("Male", enum=["Male", "Female", "Non Binary"])
-
-
-
-
+    sex: str = Form()
+    # q_cars = int = Field(...,
+    #                      ge=0)
 
 @app.get("/application", response_class=HTMLResponse)
-@app.post("/application")#, response_class=HTMLResponse)
+#@app.post("/application", response_class=HTMLResponse)
 async def application(request: Request,
-                #application: Application,
                 ):
 
-    if request.method == "GET":
-        return templates.TemplateResponse(name="index.html",
-                                          context={
-                                          "request": request,
-                                          "genders": ["Male", "Female", "Non Binary"]
-                                          })
+    # if request.method == "GET":
+    return templates.TemplateResponse(name="index.html",
+                                      context={
+                                      "request": request,
+                                      "genders": ["M", "F"]
+                                      })
 
-    if request.method == "POST":
-        data = {
-            "Age": 20,
-            "Sex": "M",
-
+#    if request.method == "POST":
+@app.post("/score", response_class=HTMLResponse)
+async def score(request: Request,
+                application: Application,
+                ):
+    data = {
+        'PAYMENT_DAY': 5,
+        'APPLICATION_SUBMISSION_TYPE': 'Web',
+        'POSTAL_ADDRESS_TYPE': 1,
+        'SEX': application.sex,
+        'MARITAL_STATUS': 2,
+        'QUANT_DEPENDANTS': 4,
+        'STATE_OF_BIRTH': 'RJ',
+        'NACIONALITY': 1,
+        'RESIDENCIAL_STATE': 'RJ',
+        'FLAG_RESIDENCIAL_PHONE': 'Y',
+        'RESIDENCIAL_PHONE_AREA_CODE': ' ',
+        'RESIDENCE_TYPE': None,
+        'MONTHS_IN_RESIDENCE': 54,
+        'FLAG_EMAIL': 1,
+        'PERSONAL_MONTHLY_INCOME': 1200,
+        'OTHER_INCOMES': 0,
+        'FLAG_VISA': 1,
+        'FLAG_MASTERCARD': 0,
+        'FLAG_DINERS': 0,
+        'FLAG_AMERICAN_EXPRESS': 0,
+        'FLAG_OTHER_CARDS': 0,
+        'QUANT_BANKING_ACCOUNTS': 2,
+        'PERSONAL_ASSETS_VALUE': 0,
+        'QUANT_CARS': 1,
+        'COMPANY': 'Y',
+        'PROFESSIONAL_STATE': 'RJ',
+        'FLAG_PROFESSIONAL_PHONE': 'N',
+        'PROFESSIONAL_PHONE_AREA_CODE': 384,
+        'MONTHS_IN_THE_JOB': 40,
+        'PROFESSION_CODE': 11,
+        'OCCUPATION_TYPE': 1,
+        'PRODUCT': 2,
+        'AGE': 30,
+        'RESIDENCIAL_ZIP_3': None,
         }
-        return data
 
+    # Send job to ml_service and receive results
+    prediction, score = model_predict(data)
+    context = {
+        "request": request,
+        "prediction": prediction,
+        "score": score
+    }
+
+    # return {"Prediction": prediction, "Score": score}
+    return templates.TemplateResponse(name="score.html",
+                                      context=context
+                                      )
             # return templates.TemplateResponse(name="index.html",
             #                                   context={"request":request})
 
@@ -59,3 +106,5 @@ async def application(request: Request,
         # return templates.TemplateResponse(name="index.html",
         #                                   context=context)
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
